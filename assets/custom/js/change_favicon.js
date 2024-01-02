7222c42606799f759fb6e36fd719fb27bc40fa3e
Saskya <135563763+byFrench@users.noreply.github.com>
2024-01-02 22:46:22 +0100

    Correction & Custom Status Sites Icon

--- assets/custom/js/change_favicon.js
+++ assets/custom/js/change_favicon.js
@@ -1,16 +1,14 @@
 window.onload = function(e){
-    var link = document.querySelector("link[rel~='icon']");
+    var link = document.querySelector("link[type='image/png']");
     var svg = document.querySelector("link[type='image/svg']");
     if (!link) {
         link = document.createElement('link');
         link.rel = 'icon';
         document.head.appendChild(link);
     }
-
     if (svg) {
         svg.remove();
-        link.href = `${window.location.href}/custom/img/favicon.png`;
-    } else {
-        link.href = `${window.location.href}/custom/img/favicon.png`;
     }
+
+    link.href = `${window.location.href}custom/img/favicon.png`;
 }
